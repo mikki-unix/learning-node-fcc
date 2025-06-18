b542308d91c0ae23ec9903ab1b205cad77ccbd14
mikki-unix <mikki.anjos@sptech.school>
2025-06-18 11:31:54 -0300

    package.json

--- 01-intro/6_exports_diferentes.js
+++ 01-intro/6_exports_diferentes.js
@@ -3,13 +3,13 @@
 module.exports.frutas = ["maçã", "banana", "laranja", "caqui"];
 
 const pessoa = {
-    nome: "Renato",
-    anoNasc: 1990,
-    idade: function (ano = 0) {
-        return ano < this.anoNasc ? 
-            new Date().getFullYear() - this.anoNasc
-            : ano - this.anoNasc;
-    }
+  nome: "Renato",
+  anoNasc: 1990,
+  idade: function (ano = 0) {
+    return ano < this.anoNasc
+      ? new Date().getFullYear() - this.anoNasc
+      : ano - this.anoNasc;
+  },
 };
 
 module.exports.Renato = pessoa;
